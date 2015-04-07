c4924d2ff639d2bd88fe93af3c2b8787c75461d6
Lars Andersen <expez@expez.com>
2015-04-07 12:09:26 +0200

    init

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject str "0.1.0-SNAPSHOT"
+  :description "String manipulation library for clojure"
+  :url "http://github.com/expez/str"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]}})

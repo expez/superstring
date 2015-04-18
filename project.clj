6bc96618a62ac8b05fca4ae35c346d0cab1b48cb
expez <expez@expez.com>
2015-04-18 09:30:27 +0200

    Use codox to generate docs

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[codox "0.8.11"]]
+  :codox {:src-dir-uri "http://github.com/expez/str/blob/master/"
+          :src-linenum-anchor-prefix "L"}
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]}})

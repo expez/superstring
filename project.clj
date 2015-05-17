ee0bf9f29ecae32815b615ab91134b090870a5e2
Lars Andersen <expez@expez.com>
2015-05-17 14:27:20 +0200

    Change license to EPL

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject str "0.1.0-SNAPSHOT"
   :description "String manipulation library for clojure"
   :url "http://github.com/expez/str"
-  :license {:name "MIT License"
-            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :year 2015
+            :key "epl-1.0"}
   :plugins [[codox "0.8.11"]]
   :codox {:src-dir-uri "http://github.com/expez/str/blob/master/"
           :src-linenum-anchor-prefix "L"}

b4a15e716ce6b5061f713b9566d042aaff017411
Lars Andersen <expez@expez.com>
2015-05-17 14:22:58 +0200

    Don't provide our own version of clojure

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "http://github.com/expez/str"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[codox "0.8.11"]]
   :codox {:src-dir-uri "http://github.com/expez/str/blob/master/"
           :src-linenum-anchor-prefix "L"}
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]
-                   :repl-options {:init-ns str.core}}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [org.clojure/test.check "0.7.0"]]
+                   :repl-options {:init-ns str.core}}
+             :provided {:dependencies [[org.clojure/clojure "1.6.0"]]}})

a78b5f5dfcb7f731db15987df5b1637657028d73
Lars Andersen <expez@expez.com>
2015-06-17 15:42:52 +0200

    Ignore convenience script

--- update-docs.sh
+++
@@ -1,9 +0,0 @@
-git checkout master &&
-    git pull &&
-    git branch -D gh-pages &&
-    git checkout -b gh-pages &&
-    lein doc &&
-    git add -f doc &&
-    git commit -m "Update docs" &&
-    git push -f origin gh-pages &&
-    git checkout master

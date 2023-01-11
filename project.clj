869c7e211d3806aa647a634c69075df735154883
Lars Andersen <expez@expez.com>
2023-01-11 17:03:36 +0100

    Next dev cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject superstring "3.1.1"
+(defproject superstring "3.2.0-SNAPSHOT"
   :description "String manipulation library for clojure and clojurescript"
   :url "http://github.com/expez/superstring"
   :license {:name "Eclipse Public License 1.0"
@@ -8,9 +8,9 @@
   :plugins [[codox "0.8.11"]]
   :codox {:src-dir-uri "http://github.com/expez/superstring/blob/master/"
           :src-linenum-anchor-prefix "L"}
-  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.0"]
-                                  [org.clojure/clojure "1.10.1"]
-                                  [org.clojure/clojurescript "1.10.520"]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]
+                                  [org.clojure/clojure "1.11.1"]
+                                  [org.clojure/clojurescript"1.11.60" ]
                                   [cider/piggieback "0.5.3"]]
                    :repl-options {:init-ns superstring.core
                                   :nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}})
